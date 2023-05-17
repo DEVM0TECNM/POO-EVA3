1ab9cebff2b41b21fcae7459be166a83af77a74b
Arturo Gerardo Banderas Sandoval <124701514+DEVM0TECNM@users.noreply.github.com>
2023-05-17 10:44:45 -0700

    Add files via upload

---
+++ EVA3_6_FINALLY/src/eva3_6_finally/EVA3_6_FINALLY.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_6_finally;
+
+public class EVA3_6_FINALLY {
+
+    public static void main(String[] args) {
+        
+        int x = 9, y = 0;
+        try{
+            int resu = x/y;
+            System.out.println("Resultado: " + resu);
+        } catch(ArithmeticException e){
+            System.out.println("No se puede dividir entre 0");
+        } finally{ //obliga a la ejecucion de est seccion de codigo es opcional
+            System.out.println("Siempre me voy a ejecutar");
+        }
+        System.out.println("FIN DEL PROGRAMA");
+    }
+}
